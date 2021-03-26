c6cccd47b94850e704e9e32e4aa56354457035f8
luckHero <59915186+luckHero@users.noreply.github.com>
2021-03-26 12:05:58 +0800

    添加键盘处理,根据键盘上下按键处理,判断坦克移动的方向

--- src/com/hyq/tank/T.java
+++ src/com/hyq/tank/T.java
@@ -6,8 +6,9 @@ import java.awt.event.WindowEvent;
 
 public class T {
     public static void main(String[] args) throws InterruptedException {
+        //创建frame类,绘制画笔
         TankFrame tankFrame = new TankFrame();
-        while (true){
+        while (true) {
             Thread.sleep(1000);
             tankFrame.repaint();
         }
--- src/com/hyq/tank/TankFrame.java
+++ src/com/hyq/tank/TankFrame.java
@@ -15,7 +15,7 @@ public class TankFrame extends Frame {
         setTitle("tank Game");//设置窗口的title
         setVisible(true);//设置窗口是否可见
         //添加键盘某个键盘监听的方法
-        this.addKeyListener( new MyListener());
+        this.addKeyListener(new MyListener());
         //为当前类添加关闭窗口的监听事件,
         addWindowListener(new WindowAdapter() {
             @Override
@@ -35,24 +35,69 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         System.out.println("调用了paint 方法");
         g.fillRect(x, y, 60, 60);//改变方块的大小
-       x += 50;
+        //x += 50;
 //        y += 50;
 
     }
+
     //处理键盘监听的方法
     class MyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bR = false;
+        boolean bU = false;
+        boolean bD = false;
+
         @Override
         //键盘被按下时调用
         public void keyPressed(KeyEvent e) {
-            System.out.println("键盘按下的方法...111");
-            x+=200;
-          //  repaint();//从新调用画布
+            int keyCode = e.getExtendedKeyCode();
+            System.out.println("键盘按下的方法...111" + keyCode);
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT: //向左
+                    bL = true;
+                    //x-=20;
+                    System.out.println(x + ":" + y);
+                    break;
+                case KeyEvent.VK_RIGHT://向右
+                    //x+=20;
+                    bR = true;
+                    System.out.println(x + ":" + y);
+                    break;
+                case KeyEvent.VK_UP: //向上
+                    // y-=20;
+                    bU = true;
+                    System.out.println(x + ":" + y);
+                    break;
+                case KeyEvent.VK_DOWN://向下
+                    //y+=20;
+                    bD = true;
+                    System.out.println(x + ":" + y);
+                    break;
+
+            }
+            //  x += 200;
+            //  repaint();//从新调用画布
         }
 
         @Override
         //键盘被放开
         public void keyReleased(KeyEvent e) {
-            System.out.println("键盘放开的方法....222");
+            int keyCode = e.getExtendedKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT: //向左
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT://向右
+                    bR = false;
+                    break;
+                case KeyEvent.VK_UP: //向上
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN: //向上
+                    bD = false;
+                    break;
+            }
+            System.out.println("键盘放开的方法....222" + keyCode);
         }
     }
 
